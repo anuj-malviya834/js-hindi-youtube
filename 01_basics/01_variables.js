0ae85a38241e8f275fec93657e707990fdad29ff
anuj-malviya834 <anujmalviya834@gmail.com>
2024-09-23 08:06:35 +0000

    updaated 01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1454
+let accountEmail = "anujmalviya@gmail.com"
+var accountPassword = "12345"
+accountcity = "jaipur"
+let state;
+var capital;
+
+// accountId = 2
+accountEmail = "anujbatsman@gmail.com"
+accountPassword = "698"
+accountcity = "pune"
+
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountcity);
+
+console.table([accountId ,accountEmail,accountPassword,accountcity,state,capital])
