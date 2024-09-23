47977f9c1df741a205b7db712eaaf3dd7bcc7adf
anuj-malviya834 <anujmalviya834@gmail.com>
2024-09-23 08:45:54 +0000

    updated dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // use for treat as a newer version of js
+// alert(3+3)
+console.log(3+3)
+
+let name="anuj"
+let age=36
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof Boolean);
+console.log(typeof String);
+console.log(typeof Object);
+console.log(typeof Symbol);
