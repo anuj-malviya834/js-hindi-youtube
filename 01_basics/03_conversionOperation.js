9d30c6f446627b95e4e645e7e22c16cc8bc81628
anuj-malviya834 <anujmalviya834@gmail.com>
2024-09-25 09:05:28 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+// // let score=Object
+
+
+// console.log(typeof score);
+// console.log(typeof (score ));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+// 
+
+let loggedIn = 0
+let isLoggedIn = Boolean(loggedIn)
+console.log(isLoggedIn);
+console.log(typeof isLoggedIn);
+
