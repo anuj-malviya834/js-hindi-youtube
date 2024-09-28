5ca2060f031521e8385537dd5e21c5f52ab728e6
anuj-malviya834 <anujmalviya834@gmail.com>
2024-09-28 06:28:26 +0000

    updated conversion file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,16 +1,36 @@
-// // let score=Object
+//  let score=""
 
 
-// console.log(typeof score);
+//  console.log(typeof score);
 // console.log(typeof (score ));
 
 // let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 // console.log( valueInNumber);
-// 
 
-let loggedIn = 0
-let isLoggedIn = Boolean(loggedIn)
-console.log(isLoggedIn);
-console.log(typeof isLoggedIn);
+
+// let loggedIn = 0
+// let isLoggedIn = Boolean(loggedIn)
+// console.log(isLoggedIn);
+// console.log(typeof isLoggedIn);
+
+// let value = -1
+// let negativeValue = -value
+// console.log(negativeValue);
+// console.log(typeof negativeValue);
+
+// let str1 = "hello"
+// let str2 = " Anuj"
+// let str3 = str1 + str2
+// console.log( str3);
+// console.log( 1 + 2 + "3" + 2 + 2);
+// console.log( 1 + 2 * 3 % 4);
+
+// console.log(-true);
+// console.log(+false);
+ 
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
 
