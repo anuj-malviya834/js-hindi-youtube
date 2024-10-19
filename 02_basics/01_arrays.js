c7a2cf9d1a3e984d57380b1d46a145cbef91694a
anuj-malviya834 <anujmalviya834@gmail.com>
2024-10-19 14:09:02 +0000

    commiting 1st lecture

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,48 @@
+//arrays
+
+const myArr = [0,1,2,3,4]
+
+const myArr2 = new Array(2,3,4,5)
+
+const myHeros = ["papa" , "mummy"]
+
+// console.log(myArr);
+// console.log(Arr[1]);
+// console.log(myHeros);
+
+
+//array methods
+
+// myArr.push(6,7)
+// console.log(myArr);
+
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(2)
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(2));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("c ", myArr);
+
+
